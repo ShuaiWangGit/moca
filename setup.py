5b057653ca9e6e0d62d116817e8b1d90ec6228b8
Shuai <sudapink@gmail.com>
2016-04-10 20:10:19 -0700

    set up initialization code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='moca',
+      version='0.1',
+      description='get and clean MOCA data newzealand',
+      url='https://github.com/ShuaiGitHub/moca',
+      author='Shuai Wang',
+      author_email='sudapink@yeah.net',
+      license='MIT',
+      packages=['moca'],
+      zip_safe=False)
